e8fbf9cc3497bca89b60f48e927ae629854b105f
Iosh Ramos <rsramos18@gmail.com>
2018-06-25 22:17:21 +0800

    add home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,78 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+providers: [
+  Camera
+]
+
+export class HomePage {
+  public base64Image: string;
+  private image: string;
+
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController, public alertCtrl: AlertController, private camera: Camera) {
 
   }
 
+  presentActionSheet() {
+   let actionSheet = this.actionSheetCtrl.create({
+     title: 'Modify your album',
+     buttons: [
+       {
+         text: 'Destructive',
+         role: 'destructive',
+         handler: () => {
+           console.log('Destructive clicked');
+         }
+       },
+       {
+         text: 'Archive',
+         handler: () => {
+           console.log('Archive clicked');
+         }
+       },
+       {
+         text: 'Cancel',
+         role: 'cancel',
+         handler: () => {
+           console.log('Cancel clicked');
+         }
+       }
+     ]
+   });
+
+   actionSheet.present();
+ }
+
+ showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+ }
+
+ onTakePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      saveToPhotoAlbum: true,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.image = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+        
+      });
+  }
 }
